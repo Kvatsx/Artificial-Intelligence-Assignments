cc3864bc01e5db4ce748f60046dfa42fc2f4e56d
kaustav16048@iiitd.ac.in <kaustav16048@iiitd.ac.in>
2018-11-10 15:28:47 +0530

    Aco written

---
+++ Assignment-3/AcoTsp/AntColonyOptimization.py
@@ -0,0 +1,101 @@
+# Kaustav Vats (2016048)
+
+from PopulateGraph import Populate
+from Ant import Ant
+import networkx as nx
+
+
+class AntColonyOptimization:
+    Pheromone = 1.0
+    EvapRate = 0.5
+    Alpha = 0.4
+    Beta = 0.6
+    Iterations = 100
+
+    def __init__(self, n):
+        self.N = n
+        self.Ants = []
+        self.GraphEdges = []
+        self.CityPheromone = []
+        self.Initialize()
+
+    def Initialize(self):
+        pop = Populate(self.N)
+        self.GraphEdges = pop.Crr
+        self.EdgePheromone = [0] * len(self.GraphEdges)
+        self.antPopulate()
+
+    def antPopulate(self):
+        for i in range(self.N):
+            AntObject = Ant(i, self.N)
+            self.Ants.append(AntObject)
+
+    def getPathDistance(self, start, stop):
+        for i in range(len(self.GraphEdges)):
+            edge = self.GraphEdges[i]
+            if edge[0] == start and edge[1] == stop:
+                return (edge[2], i)
+            elif edge[0] == stop and edge[1] == start:
+                return (edge[2], i)
+
+    def PheromoneIntensity(self, i, j, tempPeromone):
+        update = self.getPathDistance(i, j)
+        distance = update[0]
+        index = update[1]
+        ph = AntColonyOptimization.Pheromone/distance
+        tempPeromone[index] = [AntColonyOptimization.EvapRate*self.EdgePheromone[index] + ph, distance, index]
+
+
+    def NextCity(self, index):
+        CurrentAnt = self.Ants[index]
+        MaxProbCity = -1
+        MaxProbIndex = -1
+        NormalizationValue = 0.0
+        tempPheromone = [0] * len(self.GraphEdges)
+        prob = [0]*len(self.GraphEdges)
+        for i in range(self.N):
+            if not CurrentAnt.Visited[i]:
+                self.PheromoneIntensity(CurrentAnt.Location, i, tempPheromone)
+        for i in range(len(tempPheromone)):
+            NormalizationValue += (tempPheromone[i][0]**AntColonyOptimization.Alpha)*((1.0/tempPheromone[i][1])**AntColonyOptimization.Beta)
+        for i in range(len(self.GraphEdges)):
+            if not tempPheromone[i] == 0:
+                prob[i] = ((tempPheromone[i][0]**AntColonyOptimization.Alpha)*((1/tempPheromone[i][1])**AntColonyOptimization.Beta))/NormalizationValue
+                if ( prob[i] > MaxProbCity ):
+                    MaxProbCity = prob[i]
+                    MaxProbIndex = i
+        index = tempPheromone[MaxProbIndex][2]
+        CurrentAnt.updateLocation(self.GraphEdges[1])
+        return (tempPheromone[MaxProbIndex], index)
+
+
+    def updatePheromone(self, pheromone):
+        for i in range(self.N):
+            index = pheromone[i][1]
+            self.EdgePheromone[index] = pheromone[i][0]
+
+    def Reset(self):
+        for i in range(self.N):
+            self.Ants.reset(i)
+
+
+    def ShowSolution(self):
+        newGraph = []
+        for i in range(len(self.GraphEdges)):
+            newGraph.append((self.GraphEdges[i][0], self.GraphEdges[i][1], self.EdgePheromone[i]))
+        Populate.ShowGraph(nx.Graph(), newGraph)
+
+    def run(self):
+        for i in range(AntColonyOptimization.Iterations):
+            for k in range(self.N):
+                Pheromone = [0]*self.N
+                for j in range(self.N):
+                    Pheromone[i] = self.NextCity(j)
+                self.updatePheromone(Pheromone)
+            self.Reset()
+
+        self.ShowSolution()
+
+if __name__ == "__main__":
+    aco = AntColonyOptimization(4)
+    aco.run()
--- Assignment-3/AcoTsp/PopulateGraph.py
+++ Assignment-3/AcoTsp/PopulateGraph.py
@@ -21,24 +21,24 @@ class Populate:
                     self.Crr.append((i, j, z))
 
         print("Crr: ", self.Crr)
-        self.G.add_weighted_edges_from(self.Crr)
-        self.ShowGraph()
+        self.ShowGraph(self.G, self.Crr)
 
-    def ShowGraph(self):
+    def ShowGraph(self, graph, crr):
+        graph.add_weighted_edges_from(crr)
         # pos = nx.shell_layout(self.G)
         # pos = nx.kamada_kawai_layout(self.G)
         # pos = nx.rescale_layout(self.G)
-        pos = nx.spring_layout(self.G)
+        pos = nx.spring_layout(graph)
         # pos = nx.spectral_layout(self.G)
         # pos = nx.random_layout(self.G)
-        nx.draw_networkx_nodes(self.G, pos, node_size=100)
-        nx.draw_networkx_labels(self.G, pos, font_size=13, font_family='sans-serif')
-        labels = nx.get_edge_attributes(self.G, 'weight')
-        nx.draw_networkx_edge_labels(self.G, pos, edge_labels=labels, label_pos=0.3)
+        nx.draw_networkx_nodes(graph, pos, node_size=100)
+        nx.draw_networkx_labels(graph, pos, font_size=13, font_family='sans-serif')
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels, label_pos=0.3)
 
-        nx.draw(self.G, pos)
+        nx.draw(graph, pos)
         plt.axis('off')
-        plt.show()
+        # plt.show()
 
 
 if __name__ == "__main__":
